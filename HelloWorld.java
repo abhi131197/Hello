be1c7a7bfcda4fc449631a3aa17a478acb153af6
Abhishek Soni <soniabhi1311@gmail.com>
2018-09-17 20:23:40 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-import HelloWorld;
-public class HelloWorld{
-	public static void main(String[] args) {
-		System.out.println(message());
-	}
-	public static message(){
-		return "HelloWorld";
-	}
-}
